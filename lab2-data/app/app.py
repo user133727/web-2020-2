2453e219e364985d5510a97dd19d8c30a983601c
negedja <efr27@mail.ru>
2020-12-15 13:51:40 +0300

    final lab2

--- lab2-data/app/app.py
+++ lab2-data/app/app.py
@@ -28,6 +28,34 @@ def cookies():
 def form():
     return render_template('form.html')
 
-@app.route('/error_form', methods=['GET', 'POST'])
-def error_form():
-    return render_template('error_form.html')
+@app.route('/error_msg', methods=['GET', 'POST'])
+def phone():
+    format_num = ''
+    error_msg = ''
+    num = request.form.get('tel_num')
+    if num != None:
+        if num.find('-') != -1: 
+            num=num.replace('-','')
+        if num.find('(') != -1: 
+            num=num.replace('(','')
+        if num.find(')') != -1: 
+            num=num.replace(')','') 
+        if num.find('.') != -1: 
+            num=num.replace('.','') 
+        if num.find('+') != -1: 
+            num=num.replace('+','')  
+        if num.find(' ') != -1: 
+            num=num.replace(' ','')  
+        if num.isdigit():
+            if len(num) == 10:
+                format_num = '8-{}-{}-{}-{}'.format(num[0:3], num[3:6], num[6:8], num[8:])
+            elif len(num) == 11:
+                if num[0] =='7':
+                    format_num = '8-{}-{}-{}-{}'.format(num[1:4], num[4:7], num[7:9], num[9:])
+                elif num[0] =='8':
+                    format_num = '{}-{}-{}-{}-{}'.format(num[0], num[1:4], num[4:7], num[7:9], num[9:])    
+            else:
+                error_msg = 'Недопустимый ввод. Неверное количество цифр.'
+        else:       
+            error_msg = 'Недопустимый ввод. В номере телефона встречаются недопустимые символы.' 
+    return render_template('error_msg.html', format_num=format_num, error_msg=error_msg) 
